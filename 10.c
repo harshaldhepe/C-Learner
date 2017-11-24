96c6b0eafc2f9eeff4b908970dfb6a21f716d142
harshaldhepe <33717706+harshaldhepe@users.noreply.github.com>
2017-11-24 16:21:13 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+int b;
+printf("Enter the first no");
+scanf("%d",&a);
+printf("Enter the second no");
+scanf("%d",&b);
+int sum=a-b;
+printf("sum of entered numbers=%d",sum);
+}
